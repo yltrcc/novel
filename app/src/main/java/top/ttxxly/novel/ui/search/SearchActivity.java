80a26b4454d692835a4bdfa7c436decf0140e10c
ttxxly <ttxxly@gmail.com>
2018-03-02 22:53:19 +0800

    完善 搜索页面布局 增加 搜索热词 View。

--- app/src/main/java/top/ttxxly/novel/ui/search/SearchActivity.java
+++ app/src/main/java/top/ttxxly/novel/ui/search/SearchActivity.java
@@ -3,6 +3,7 @@ package top.ttxxly.novel.ui.search;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.gujun.android.taggroup.TagGroup;
 import top.ttxxly.novel.R;
 
 public class SearchActivity extends AppCompatActivity {
@@ -11,5 +12,8 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        TagGroup mTagGroup = findViewById(R.id.TGSearch);
+        mTagGroup.setTags(new String[]{"影帝的老婆", "花豹突击队", "一言不合就卖萌", "最强武神", "大数据修仙", "天才高手"});
     }
 }
